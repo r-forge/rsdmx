6399f7d43c97b6c38a40a56eb9eac0b66fe2777d
eblondel (Emmanuel Blondel) <emmanuel.blondel1@gmail.com>
2012-05-09 20:53:48 +0000

    - getCodeListNames.r modified (see bug #1989) Previous status: the function assumed the presence of codelists into the DSD, through a <message:CodeList> XML node.
    Improvement: The codelist names are now retrieved from the the <message:KeyFamilies> XML node. 
    
    TODO:
    - discuss the potential overlaps between R functions: e.g. getCodeListNames.R vs. summary(sdmx)
    - identify if/when the entire codelists are part of the DSD or not
    (such as the ESS_DSDFAOSTAT.xml example). Curent DSD provided in FAO FIGIS SDMX do not provide the codelists as part of the DSD.
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rsdmx@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/getCodeListNames.r
+++ pkg/R/getCodeListNames.r
@@ -1,13 +1,39 @@
 ## Author: Emmanuel Blondel
 ## Contact: emmanuel.blondel1 at gmail.com
-## Creation: 05/05/2011
-## Last update: -
+## Created on 05/05/2011
+## Last Update:09/05/2012
+## Type: Function
+## Name: getCodeListNames.R
+## Description: A R function to retrieve the codelist names (dimension, attribute or all) from a DSD
+##			  (SDMX Data Structure Definition). When retrieving all codelists, these can be retrieved
+##			  in a list distinguishing both "dimension" vs. "attribute" codelists
+## =================================================================================================
 
-######### getCodeListNames(sdmx) ###################
+getCodeListNames<-function(sdmx, type=c('all','dimension','attribute'), split = FALSE){
+	
+	# type argument check
+	type<-match.arg(type)
 
-getCodeListNames<-function(sdmx)
-{
-listNamesTP<-getNodeSet(sdmx,"//structure:CodeList")
-listNames<-sapply(listNamesTP,function(x) xmlGetAttr(x,"id"))
-return(listNames)
+	# get Dimension codelists
+	listDimensionsXML<-getNodeSet(sdmx,'//structure:Dimension')
+	listDimensions<-sapply(listDimensionsXML,function(x) xmlGetAttr(x,'codelist'))
+
+	# get Attribute codelists
+	listAttrsXML<-getNodeSet(sdmx,'//structure:Attribute')
+	listAttrs<-sapply(listAttrsXML,function(x) xmlGetAttr(x,'codelist'))
+
+	# output management
+	if(missing(type) || type=='all'){
+		if(split==FALSE){
+			listNames<-append(listDimensions, listAttrs)
+		}else{
+			listNames<-list(dimensions = listDimensions, attributes = listAttrs)
+		}
+	}else if(type=='dimension'){
+		listNames<-listDimensions
+	}else if(type=='attribute'){
+		listNames<-listAttrs
+	}
+	
+	return(listNames)
 }
