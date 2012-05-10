cfd32beb088dbc551ae6025e3090b321e1297776
eblondel (Emmanuel Blondel) <emmanuel.blondel1@gmail.com>
2012-05-10 15:24:10 +0000

    - getCodeListNames.r modified to distinguish if it applies to a RSDMXKeyFamilies vs. RSDMXCodeList object. In case of a RSDMXCodeList, the codelist name(s) must be parsed from the <structure:CodeList> tag ("id" attribute"). Solves the issue [#1994], at least temporarily.
    - add exampleData/demography/demography_2.xml: illustrates why getCodeListNames.r & getCodeList.r do no work with this file. In the native file exampleData/demography/demography.xml, the tag is <structure:Codelist> while it is generally <structure:CodeList> (uppercase "L" for list). Deals with the SDMX/ML schema used for the file... Using demography_2.xml, the getCodeListNames.r is successful, while for the getCodeList.r, other similar issues of XML tags still prevent from getting codelist content.
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rsdmx@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/getCodeListNames.r
+++ pkg/R/getCodeListNames.r
@@ -1,39 +1,49 @@
 ## Author: Emmanuel Blondel
 ## Contact: emmanuel.blondel1 at gmail.com
 ## Created on 05/05/2011
-## Last Update:09/05/2012
+## Last Update:10/05/2012
 ## Type: Function
 ## Name: getCodeListNames.R
 ## Description: A R function to retrieve the codelist names (dimension, attribute or all) from a DSD
 ##			  (SDMX Data Structure Definition). When retrieving all codelists, these can be retrieved
 ##			  in a list distinguishing both "dimension" vs. "attribute" codelists
+##			  For RSDMXCodeLists R objects, it uses the structure:CodeList "id" attribute
 ## =================================================================================================
 
 getCodeListNames<-function(sdmx, type=c('all','dimension','attribute'), split = FALSE){
 	
-	# type argument check
-	type<-match.arg(type)
-
-	# get Dimension codelists
-	listDimensionsXML<-getNodeSet(sdmx,'//structure:Dimension')
-	listDimensions<-sapply(listDimensionsXML,function(x) xmlGetAttr(x,'codelist'))
-
-	# get Attribute codelists
-	listAttrsXML<-getNodeSet(sdmx,'//structure:Attribute')
-	listAttrs<-sapply(listAttrsXML,function(x) xmlGetAttr(x,'codelist'))
-
-	# output management
-	if(missing(type) || type=='all'){
-		if(split==FALSE){
-			listNames<-append(listDimensions, listAttrs)
-		}else{
-			listNames<-list(dimensions = listDimensions, attributes = listAttrs)
-		}
-	}else if(type=='dimension'){
-		listNames<-listDimensions
-	}else if(type=='attribute'){
-		listNames<-listAttrs
-	}
+	if(class(sdmx)[1] == "RSDMXKeyFamilies"){
 	
+		# type argument check
+		type<-match.arg(type)
+
+		# get Dimension codelists
+		listDimensionsXML<-getNodeSet(sdmx,'//structure:Dimension')
+		listDimensions<-sapply(listDimensionsXML,function(x) xmlGetAttr(x,'codelist'))
+
+		# get Attribute codelists
+		listAttrsXML<-getNodeSet(sdmx,'//structure:Attribute')
+		listAttrs<-sapply(listAttrsXML,function(x) xmlGetAttr(x,'codelist'))
+
+		# output management
+		if(missing(type) || type=='all'){
+			if(split==FALSE){
+				listNames<-append(listDimensions, listAttrs)
+			}else{
+				listNames<-list(dimensions = listDimensions, attributes = listAttrs)
+			}
+		}else if(type=='dimension'){
+			listNames<-listDimensions
+		}else if(type=='attribute'){
+			listNames<-listAttrs
+		}
+	}else if(class(sdmx)[1]== "RSDMXCodeLists"){
+		
+		# get codelist
+		listNamesXML<-getNodeSet(sdmx,'//structure:CodeList')
+		listNames<-sapply(listNamesXML,function(x) xmlGetAttr(x,'id'))
+
+	}
+		
 	return(listNames)
 }
